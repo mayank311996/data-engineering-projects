b26a0e12d1b76d50f02c86dc85a7041f8f70c768
mayank311996 <mayankvadsola1996@gmail.com>
2020-12-10 21:43:41 -0500

    editing sql_queries.py
    - Adding queries to insert values in tables
    - Appending queries in lists one from create and one for drop

--- data_modeling_with_postgres_Udacity/src/sql_queries.py
+++ data_modeling_with_postgres_Udacity/src/sql_queries.py
@@ -1,3 +1,4 @@
+##############################################################################
 # Queries to drop tables
 
 songplay_table_drop = "DROP TABLE IF EXISTS songplays"
@@ -6,6 +7,7 @@ song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
+##############################################################################
 # Queries to create tables
 
 songplay_table_create = ("""
@@ -63,4 +65,98 @@ time_table_create = ("""
         weekday VARCHAR NOT NULL
     )
 """)
+
+##############################################################################
+# Queries to insert records
+# Note: Some insert queries have CONFLICT statement to account for modification
+
+songplay_table_insert = ("""
+    INSERT INTO songplays VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s)
+""")
+
+user_table_insert = ("""
+    INSERT INTO users (user_id, first_name, last_name, gender, level) 
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id)
+    DO UPDATE 
+    SET level = EXCLUDED.level
+""")
+
+song_table_insert = ("""
+    INSERT INTO songs (song_id, title, artist_id, year, duration)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id)
+    DO NOTHING 
+""")
+
+artist_table_insert = ("""
+    INSERT INTO artists (artist_id, name, location, latitude, longitude)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id)
+    DO UPDATE 
+    SET location = EXCLUDED.location,
+    latitude = EXCLUDED.latitude,
+    longitude = EXCLUDED.longitude 
+""")
+
+time_table_insert = ("""
+    INSERT INTO time 
+    VALUES (%s, %s, %s, %s, %s, %s, %s) 
+    ON CONFLICT (start_time)
+    DO NOTHING 
+""")
+
+##############################################################################
+# Query to find songs
+
+song_select = ("""
+    SELECT song_id, artists.artist_id
+    FROM songs 
+    JOIN artists ON songs.artist_id = artists.artist_id
+    WHERE songs.title = %s
+    AND artists.name = %s
+    AND songs.duration = %s
+""")
+
+##############################################################################
+# Query lists
+
+create_table_queries = [
+    songplay_table_create,
+    user_table_create,
+    song_table_create,
+    artist_table_create,
+    time_table_create
+]
+
+drop_table_queries = [
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
