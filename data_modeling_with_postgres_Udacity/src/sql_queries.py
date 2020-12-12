51f21688b965b02ba1d56b7ab8e3a29dff81a399
mayank311996 <mayankvadsola1996@gmail.com>
2020-12-12 14:54:21 -0500

    editing sql_queries.py
    - changing order to avoid reference error

--- data_modeling_with_postgres_Udacity/src/sql_queries.py
+++ data_modeling_with_postgres_Udacity/src/sql_queries.py
@@ -122,11 +122,11 @@ song_select = ("""
 # Query lists
 
 create_table_queries = [
-    songplay_table_create,
     user_table_create,
-    song_table_create,
     artist_table_create,
-    time_table_create
+    song_table_create,
+    time_table_create,
+    songplay_table_create
 ]
 
 drop_table_queries = [
