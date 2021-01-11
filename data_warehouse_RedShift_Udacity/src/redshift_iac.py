d4ce46858e7e1c59d3260201b7bd7f7b2c017371
mayank311996 <mayankvadsola1996@gmail.com>
2021-01-11 12:01:52 -0500

    editing redshift_iac.py
    - adding delete_IAM_role function

--- data_warehouse_RedShift_Udacity/src/redshift_iac.py
+++ data_warehouse_RedShift_Udacity/src/redshift_iac.py
@@ -93,3 +93,97 @@ def create_IAM_role(iam_client):
                                ['HTTPStatusCode'] == 200)) else False
 
 
+def delete_IAM_role(iam_client):
+    """
+    Delete and IAM role Make sure that you do not have any Amazon EC2
+    instances running with the role you are about to delete. Deleting a role
+    or instance profile that is associated with a running instance will
+    break any applications running on the instance.
+    :param iam_client: an IAM service client instance
+    :return: True if role deleted successfully.
+    """
+
+    role_name = config.get('IAM_ROLE', 'NAME')
+
+    existing_roles = [
+        role['RoleName'] for role in iam_client.list_roles()['Roles']
+    ]
+    if role_name not in existing_roles:
+        logger.info(f"Role {role_name} does not exist.")
+        return True
+
+    logger.info(f"Processing deleting IAM role : {role_name}")
+    try:
+        detach_response = iam_client.detach_role_policy(
+            RoleName=role_name, PolicyArn=config.get('IAM_ROLE', 'POLICY_ARN')
+        )
+        logger.debug(
+            f"Response for policy detach from IAM role : {detach_response}"
+        )
+        logger.info(
+            f"Detach policy response code "
+            f": {detach_response['ResponseMetadata']['HTTPStatusCode']}"
+        )
+        delete_response = iam_client.delete_role(RoleName=role_name)
+        logger.debug(
+            f"Response for deleting IAM role : {delete_response}"
+        )
+        logger.info(
+            f"Delete role response code "
+            f": {delete_response['ResponseMetadata']['HTTPStatusCode']}"
+        )
+    except Exception as e:
+        logger.error(
+            f"Exception occured while deleting role : {e}"
+        )
+        return False
+
+    return True if ((detach_response['ResponseMetadata']['HTTPStatusCode'] ==
+                     200) and (delete_response['ResponseMetadata']
+                               ['HTTPStatusCode'] == 200)) else False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
