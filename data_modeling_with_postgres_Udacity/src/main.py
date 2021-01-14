d0a2f5f99aaf89cee7722fb22d4c1a03cab74cc3
mayank311996 <mayankvadsola1996@gmail.com>
2021-01-14 10:27:16 -0500

    editing main.py

--- data_modeling_with_postgres_Udacity/src/main.py
+++ data_modeling_with_postgres_Udacity/src/main.py
@@ -2,6 +2,9 @@ from create_tables import main as create_tables_main
 from etl import main as etl_main
 
 
+##############################################################################
 if __name__ == '__main__':
     create_tables_main()
+    print("Tables created successfully")
     etl_main()
+    print("data inserted successfully")
